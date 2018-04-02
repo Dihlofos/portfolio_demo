4425f15a762a9a7548008d44648bd8a448b497f4
samsonov <samsonov.ibrush@gmail.com>
2018-04-02 19:23:42 +0300

    parallax done

--- build/assets/js/main.js
+++ build/assets/js/main.js
@@ -1,10 +1,58 @@
-"use strict";
+'use strict';
 
 svg4everybody();
 
 var APP = {};
 $(function () {
-	console.log("iBrush HTML Starter");
-	console.log(APP);
+
+	$('.js-parallax-first').parallax({
+		imageSrc: 'assets/img/bg-samsung-tab.jpg',
+		speed: 0
+	});
+
+	$(window).on("scroll", function () {
+		var vh = $(window).height();
+		if ($(window).scrollTop() > vh - $(".header").height()) {
+			$(".header").attr("style", "transform: translateY(-100px);");
+		} else {
+			$(".header").attr("style", "transform: translateY(0);");
+		}
+		if ($(window).scrollTop() > 0) {
+			$(".parallax-slider").addClass("blur");
+		} else {
+			$(".parallax-slider").removeClass("blur");
+		}
+	});
+
+	function Scrolling(item, offset) {
+		this.item = item;
+		this.position;
+		this.trigger = Math.round(item.offset().top - offset) > 0 ? Math.round(item.offset().top - offset) : 10;
+		this.init();
+	}
+
+	Scrolling.prototype = {
+		init: function init() {
+			this.bindScroll();
+		},
+		bindScroll: function bindScroll() {
+			var obj = this;
+
+			$(window).on("scroll", function () {
+				obj.position = Math.round($(window).scrollTop());
+				if (obj.position > obj.trigger) {
+					$(obj.item).addClass("fade-out");
+				}
+				if (obj.position < obj.trigger) {
+					$(obj.item).removeClass("fade-out");
+				}
+			});
+		}
+	};
+
+	var appears = [];
+	$(".js-appear").each(function (index) {
+		appears[index] = new Scrolling($(this), 130);
+	});
 });
 //# sourceMappingURL=main.js.map
--- src/js/vendor.js
+++ src/js/vendor.js
@@ -1,3 +1,4 @@
 //=require svg4everybody/dist/svg4everybody.min.js
 //=require jquery/dist/jquery.min.js
 //=require slick-carousel/slick/slick.min.js
+//=require jquery-parallax.js/parallax.min.js
