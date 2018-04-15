34e7ffc2a7633f242474b6106868e853d90de660
samsonov <samsonov.ibrush@gmail.com>
2018-04-15 20:59:08 +0300

    done with bugs

--- src/js/scripts/scripts.js
+++ src/js/scripts/scripts.js
@@ -7,6 +7,7 @@ $(function() {
 			naturalHeight: 600
 		}
 	);
+	$('.js-parallax-first').attr("style", " ");
 
 	$('.slider').slick({
 	  dots: true,
@@ -20,11 +21,23 @@ $(function() {
 	$(window).on("scroll", function(){		
 		var vh = $(window).height();
 		var scrollState = false;
+
 		if ($(window).scrollTop() > (vh - $(".header").height())){
 			$(".header").attr("style","transform: translateY(-100px);");
+			
 		} else {
 			$(".header").attr("style","transform: translateY(0);");
+			
 		}
+
+		if ($(window).scrollTop() > ($(".js-parallax-first").height())){
+			$(".js-bottom").addClass("is-clickable");
+		} else {
+			$(".js-bottom").removeClass("is-clickable");
+		}
+
+
+
 		if ($(window).scrollTop() > 0  && scrollState == false){
 			$(".parallax-slider").addClass("blur");
 			scrollState = true;
@@ -63,6 +76,29 @@ $(function() {
 	var appears = [];
 	$(".js-appear").each(function(index){
 		appears[index] = new Scrolling($(this),130);
+	});
+
+	$(".header__link-cont").one(
+    "animationend  animationend  webkitAnimationEnd  oanimationend MSAnimationEnd",
+     function() {
+      $(this).removeClass("width-rise-18 width-rise-22 width-rise-26");
+      $(this).addClass("after-anim");
+     }
+   );
+
+	$(".js-bottom").on("click", function(e){
+		e.preventDefault();
+		$(this).addClass("full");
+		$(".case-single__bottom-title").remove();
+		$(".case-single__bottom-link").remove();
+		$(".case-single__bottom-bg").removeClass("blur");
+		$(this).one(
+          "transitionend",
+          function() {
+            location.href = $(this).attr("href")
+          }
+        );
+		
 	});
 
 
